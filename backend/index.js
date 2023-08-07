2fbd3972af0ae0f440ece5d5a3fca1df7b931f17
Fritzie Primananda Adi Praja <primanandafritzie@gmail.com>
2023-08-07 11:12:21 +0800

    backend folder

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000; // Port yang ingin Anda gunakan
+
+// Contoh route sederhana
+app.get('/', (req, res) => {
+  res.send('Hello from the backend!');
+});
+
+// Memulai server
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
