ed58cce230c52d133472b16d3bef3e51e66cd4cb
Fritzie Primananda Adi Praja <primanandafritzie@gmail.com>
2023-08-09 03:28:35 +0800

    make over header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,16 +1,54 @@
+// import React from 'react';
+// import { Navbar, Container, Form, FormControl, Button } from 'react-bootstrap';
+
+// const Header = () => {
+//   return (
+//     <Navbar bg="dark" variant="dark">
+//       <Container>
+//         <Navbar.Brand>My App Title</Navbar.Brand>
+//         <Form inline="true" className="ml-auto">
+//           <div className="d-flex align-items-center">
+//             <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+//             <Button variant="outline-light">Search</Button>
+//           </div>
+//         </Form>
+//       </Container>
+//     </Navbar>
+//   );
+// };
+
+// export default Header;
+
 import React from 'react';
-import { Navbar, Container, Form, FormControl, Button } from 'react-bootstrap';
+import { Navbar, Container, Nav, Form, Button } from 'react-bootstrap';
+import { BsSearch } from 'react-icons/bs'; // Import the search icon from react-icons/bs
+import imagePath from '../assets/logo.png';
 
 const Header = () => {
   return (
-    <Navbar bg="dark" variant="dark">
+    <Navbar expand="lg" className="navbar bg-white shadow navbar-expand-lg fixed-top banner--stick">
       <Container>
-        <Navbar.Brand>My App Title</Navbar.Brand>
+        <Navbar.Brand href="#">
+          <img src={imagePath} alt="Logo" />
+        </Navbar.Brand>
         <Form inline className="ml-auto">
-          <div className="d-flex align-items-center">
-            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-            <Button variant="outline-light">Search</Button>
-          </div>
+          {/* Use a regular input field with custom styles */}
+          <input
+            type="text"
+            placeholder="Search something"
+            style={{
+              border: 'none',
+              borderBottom: '1px solid #ccc',
+              padding: '5px 0',
+              marginRight: '10px',
+              outline: 'none',
+              width: '200px', // Adjust the width as needed
+            }}
+          />
+          {/* Use the search icon */}
+          <Button variant="link" style={{ backgroundColor: 'transparent', border: 'none', outline: 'none', cursor: 'pointer' }}>
+            <BsSearch />
+          </Button>
         </Form>
       </Container>
     </Navbar>
--- frontend/src/components/thumbnail.js
+++ frontend/src/components/thumbnail.js
@@ -1,102 +1,180 @@
-import React, {useEffect, useState} from 'react';
-import axios from 'axios';
-import { Card } from 'react-bootstrap';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Card } from "react-bootstrap";
 
 const Thumbnail = (video) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-    const [data, setData] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
+  // function sleep(ms) {
+  //     return new Promise((resolve) => setTimeout(resolve, ms));
+  // }
 
-    // function sleep(ms) {
-    //     return new Promise((resolve) => setTimeout(resolve, ms));
-    // }
+  useEffect(() => {
+    // Function to fetch data from the API
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:3001/api/video/videos"
+        );
+        // const response = await axios.get('http://localhost:3001/api/product/products/64d10db5389de905c3e3b891');
+        // 64d10db5389de905c3e3b891
+        console.log(JSON.stringify(response.data));
+        // console.log(data.link_thumbnail);
+        setData(response.data);
+        // await sleep(2000);
+        setLoading(false);
+      } catch (error) {
+        setError("Error fetching data " + error);
+        setLoading(false);
+      }
+    };
 
-    useEffect(() => { // Function to fetch data from the API
-        const fetchData = async () => {
-            try {
-                const response = await axios.get('http://localhost:3001/api/video/videos');
-                // const response = await axios.get('http://localhost:3001/api/product/products/64d10db5389de905c3e3b891');
-                // 64d10db5389de905c3e3b891
-                console.log("object")
-                console.log(JSON.stringify(response.data));
-                setData(response.data);
-                // await sleep(2000);
-                setLoading(false);
-            } catch (error) {
-                setError('Error fetching data ' + error);
-                setLoading(false);
-            }
-        };
+    // Call the fetch function
+    fetchData();
+  }, []); // Empty dependency array, so it runs only once on mount
 
-        // Call the fetch function
-        fetchData();
-    }, []); // Empty dependency array, so it runs only once on mount
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
+  if (error) {
+    return <div>{error}</div>;
+  }
 
-    if (loading) {
-        return <div>Loading...</div>;
-    }
+  return (
+    // <div>
+    //     <h1>Dats Display</h1>
+    //     <ul> {
+    //         data.map((item) => (
+    //             <li key={
+    //                 item._id
+    //             }>
+    //                 {/* <img src={'../assets/'+item.link_thumbnail} alt="Thumbnail" /> */}
+    //                 {/* <ImageDisplay imageName={item.link_thumbnail}></ImageDisplay> */}
+    //                 <p>Title: {
+    //                     item.title
+    //                 }</p>
+    //                 <p>Video Link: {
+    //                     item.link_video
+    //                 }</p>
+    //                 <p>Upload Date: {
+    //                     item.upload_date
+    //                 }</p>
+    //                 <button onClick={() => {
+    //                   window.location.href= "http://localhost:3000/video?id="+item._id;
+    //                   // console.log('click');
+    //                 }}>
+    //                     <img src={
+    //                             item.link_thumbnail
+    //                         }
+    //                         alt="Thumbnail"/>
+    //                 </button>
 
-    if (error) {
-        return <div>{error}</div>;
-    }
+    //                 {/* <p>Title: {item.title}</p>
+    //     <p>price: {item.price}</p>
+    //     <p>link: {item.link_product}</p> */} </li>
+    //         ))
+    //     } </ul>
+    // </div>
 
-    return (
-        // <div>
-        //     <h1>Dats Display</h1>
-        //     <ul> {
-        //         data.map((item) => (
-        //             <li key={
-        //                 item._id
-        //             }>
-        //                 {/* <img src={'../assets/'+item.link_thumbnail} alt="Thumbnail" /> */}
-        //                 {/* <ImageDisplay imageName={item.link_thumbnail}></ImageDisplay> */}
-        //                 <p>Title: {
-        //                     item.title
-        //                 }</p>
-        //                 <p>Video Link: {
-        //                     item.link_video
-        //                 }</p>
-        //                 <p>Upload Date: {
-        //                     item.upload_date
-        //                 }</p>
-        //                 <button onClick={() => {
-        //                   window.location.href= "http://localhost:3000/video?id="+item._id;
-        //                   // console.log('click');
-        //                 }}>
-        //                     <img src={
-        //                             item.link_thumbnail
-        //                         }
-        //                         alt="Thumbnail"/>
-        //                 </button>
-
-        //                 {/* <p>Title: {item.title}</p>
-        //     <p>price: {item.price}</p>
-        //     <p>link: {item.link_product}</p> */} </li>
-        //         ))
-        //     } </ul>
-        // </div>
-
-        <div className="container">
+    <div className="container">
       <div className="row">
-        {data.map(video => (
+        {data.map((video) => (
           <div className="col-md-2 mb-2" key={video._id}>
-            <Card style={{ width: '15rem' }}>
-      <Card.Img variant="top" src={video.link_thumbnail} />
-      <Card.Body>
-        <Card.Title>{video.title}</Card.Title>
-        <Card.Text>{video.upload_date}</Card.Text>
-        <a href={"http://localhost:3000/video?id="+video._id} target="_blank" rel="noopener noreferrer">Watch Video</a>
-      </Card.Body>
-    </Card>
+            <Card style={{ width: "15rem" }}>
+              <Card.Img variant="top" src={video.link_thumbnail} />
+              <Card.Body>
+                <Card.Title>{video.title}</Card.Title>
+                <Card.Text>{video.upload_date}</Card.Text>
+                <a
+                  href={"http://localhost:3000/video?id=" + video._id}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Watch Video
+                </a>
+              </Card.Body>
+            </Card>
           </div>
         ))}
-        
+        {data.map((video) => (
+          <div className="col-md-2 mb-2" key={video._id}>
+            <Card style={{ width: "15rem" }}>
+              <Card.Img variant="top" src={video.link_thumbnail} />
+              <Card.Body>
+                <Card.Title>{video.title}</Card.Title>
+                <Card.Text>{video.upload_date}</Card.Text>
+                <a
+                  href={"http://localhost:3000/video?id=" + video._id}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Watch Video
+                </a>
+              </Card.Body>
+            </Card>
+          </div>
+        ))}
+        {data.map((video) => (
+          <div className="col-md-2 mb-2" key={video._id}>
+            <Card style={{ width: "15rem" }}>
+              <Card.Img variant="top" src={video.link_thumbnail} />
+              <Card.Body>
+                <Card.Title>{video.title}</Card.Title>
+                <Card.Text>{video.upload_date}</Card.Text>
+                <a
+                  href={"http://localhost:3000/video?id=" + video._id}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Watch Video
+                </a>
+              </Card.Body>
+            </Card>
+          </div>
+        ))}
+        {data.map((video) => (
+          <div className="col-md-2 mb-2" key={video._id}>
+            <Card style={{ width: "15rem" }}>
+              <Card.Img variant="top" src={video.link_thumbnail} />
+              <Card.Body>
+                <Card.Title>{video.title}</Card.Title>
+                <Card.Text>{video.upload_date}</Card.Text>
+                <a
+                  href={"http://localhost:3000/video?id=" + video._id}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Watch Video
+                </a>
+              </Card.Body>
+            </Card>
+          </div>
+        ))}
+        {data.map((video) => (
+          <div className="col-md-2 mb-2" key={video._id}>
+            <Card style={{ width: "15rem" }}>
+              <Card.Img variant="top" src={video.link_thumbnail} />
+              <Card.Body>
+                <Card.Title>{video.title}</Card.Title>
+                <Card.Text>{video.upload_date}</Card.Text>
+                <a
+                  href={"http://localhost:3000/video?id=" + video._id}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Watch Video
+                </a>
+              </Card.Body>
+            </Card>
+          </div>
+        ))}
       </div>
     </div>
 
-    );
+  );
 };
 
 export default Thumbnail;
