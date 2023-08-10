38a9a0462269bf483338d184ea7b595a8f26d946
Fritzie Primananda Adi Praja <primanandafritzie@gmail.com>
2023-08-11 02:31:45 +0800

    list product error

--- backend/src/models/migrate.js
+++ backend/src/models/migrate.js
@@ -1,62 +1,118 @@
 // populate.js
 
 const mongoose = require("mongoose");
-const video = require('./Video');
-const product = require('./Product');
-const comment = require('./Comment');
-const user = require('./User');
+const video = require("./Video");
+const product = require("./Product");
+const comment = require("./Comment");
+const user = require("./User");
 
 const videosData = [
-    {
-        link_thumbnail: "dummy.png",
-        link_video: "https://www.youtube.com/watch?v=yoBibrIPWJQ",
-        title: "MURAH BGT",
-        upload_date: "2022-07-28",
-    },
-    {
-        link_thumbnail: "dummy.png",
-        link_video: "https://www.youtube.com/watch?v=yoBibrIPWJQ",
-        title: "MURAH BGT",
-        upload_date: "2022-07-28",
-    },
-    {
-        link_thumbnail: "dummy.png",
-        link_video: "https://www.youtube.com/watch?v=yoBibrIPWJQ",
-        title: "MURAH BGT",
-        upload_date: "2022-07-28",
-    },
+  {
+    link_thumbnail: "dummy.png",
+    link_video: "https://www.youtube.com/watch?v=yoBibrIPWJQ",
+    title: "MURAH BGT",
+    upload_date: "2022-07-28",
+  },
+  {
+    link_thumbnail: "dummy.png",
+    link_video: "https://www.youtube.com/watch?v=yoBibrIPWJQ",
+    title: "MURAH BGT",
+    upload_date: "2022-07-28",
+  },
+  {
+    link_thumbnail: "dummy.png",
+    link_video: "https://www.youtube.com/watch?v=yoBibrIPWJQ",
+    title: "MURAH BGT",
+    upload_date: "2022-07-28",
+  },
 ];
 
-async function insertVideo(){
-    await video.insertMany(videosData);
-    console.log("Videos data inserted successfully");
+const productData = [
+  {
+    link_product: `https://www.tokopedia.com/baabudstore20/sweety-bronze-perekat-m-32`,
+    title: `Mami Poko`,
+    price: 2000,
+    link_imageProduct: `product.jpg`,
+  },
+  {
+    link_product: `https://www.tokopedia.com/baabudstore20/sweety-bronze-perekat-m-32`,
+    title: `Poko Mami`,
+    price: 6000,
+    link_imageProduct: `product.jpg`,
+  },
+  {
+    link_product: `https://www.tokopedia.com/baabudstore20/sweety-bronze-perekat-m-32`,
+    title: `Mako Pomi`,
+    price: 12000,
+    link_imageProduct: `product.jpg`,
+  },
+  {
+    link_product: `https://www.tokopedia.com/baabudstore20/sweety-bronze-perekat-m-32`,
+    title: `Mami Poko`,
+    price: 2000,
+    link_imageProduct: `product.jpg`,
+  },
+  {
+    link_product: `https://www.tokopedia.com/baabudstore20/sweety-bronze-perekat-m-32`,
+    title: `Poko Mami`,
+    price: 6000,
+    link_imageProduct: `product.jpg`,
+  },
+  {
+    link_product: `https://www.tokopedia.com/baabudstore20/sweety-bronze-perekat-m-32`,
+    title: `Mako Pomi`,
+    price: 12000,
+    link_imageProduct: `product.jpg`,
+  },
+  {
+    link_product: `https://www.tokopedia.com/baabudstore20/sweety-bronze-perekat-m-32`,
+    title: `Mami Poko`,
+    price: 2000,
+    link_imageProduct: `product.jpg`,
+  },
+  {
+    link_product: `https://www.tokopedia.com/baabudstore20/sweety-bronze-perekat-m-32`,
+    title: `Poko Mami`,
+    price: 6000,
+    link_imageProduct: `product.jpg`,
+  },
+  {
+    link_product: `https://www.tokopedia.com/baabudstore20/sweety-bronze-perekat-m-32`,
+    title: `Mako Pomi`,
+    price: 12000,
+    link_imageProduct: `product.jpg`,
+  },
+];
+
+async function insertVideo() {
+  await video.insertMany(videosData);
+  console.log("Videos data inserted successfully");
 }
 
-async function insertComment(){
-    const videos = await video.find();
-    for (const Video of videos) {
-        await comment.create({
-            video_id: Video._id,
-            username: "Maleo",
-            comment: "ggwp",
-            timestamp: Date.now()
-        });
-    }  
-    console.log("Comment data inserted successfully");
+async function insertComment() {
+  const videos = await video.find();
+  for (const Video of videos) {
+    await comment.create({
+      video_id: Video._id,
+      username: "Maleo",
+      comment: "ggwp",
+      timestamp: Date.now(),
+    });
+  }
+  console.log("Comment data inserted successfully");
 }
 
-async function insertProduct(){
-    const videos = await video.find();
-    for (const Video of videos) {
-        await product.create({
-          link_product: `https://example.com/product`,
-          title: `Product Title for`,
-          price: 19.99,
-          link_imageProduct: `https://example.com/product.jpg`,
-          video_id: Video._id // Gunakan _id dari Video sebagai referensi video_id
-        });
-    }
-    console.log("Product data inserted successfully");
+async function insertProduct() {
+  const videos = await video.find();
+  for (const videoDoc of videos) {
+    const productDataWithVideoId = productData.map((productItem) => ({
+      ...productItem,
+      video_id: videoDoc._id,
+    }));
+
+    await product.insertMany(productDataWithVideoId);
+  }
+  console.log("Product data inserted successfully");
 }
 
 async function populateDatabase() {
